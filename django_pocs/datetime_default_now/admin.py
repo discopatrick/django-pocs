1f58203a36bbcd30c5f9b4d71a42d12238144d46
discopatrick <git@discopatrick.me>
2016-12-19 14:37:46 +0000

    tests the form used in the admin app

--- django_pocs/datetime_default_now/admin.py
+++ django_pocs/datetime_default_now/admin.py
@@ -1,3 +1,13 @@
+from django import forms
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post
+
+class PostAdminForm(forms.ModelForm):
+    class Meta:
+        fields = '__all__'
+        model = Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    form = PostAdminForm
--- django_pocs/datetime_default_now/tests.py
+++ django_pocs/datetime_default_now/tests.py
@@ -4,13 +4,39 @@ from datetime import datetime
 from django.utils import timezone
 
 from .models import Post
+from .admin import PostAdminForm
 
-class PostModelTest(TestCase):
+class TestCaseBase(TestCase):
+    party_like_its_1999 = timezone.make_aware(datetime(year=1999, month=12, day=31, hour=23, minute=59, second=59))
+
+class PostModelTest(TestCaseBase):
 
     def test_saving_and_retriving_a_post(self):
 
         post = Post()
-        post.datetime = timezone.make_aware(datetime(year=1999, month=12, day=31, hour=23, minute=59, second=59))
+        post.datetime = self.party_like_its_1999
         post.save()
 
         self.assertEqual(Post.objects.count(), 1)
+
+class PostAdminFormTest(TestCaseBase):
+
+    def test_empty_form_does_not_validate(self):
+
+        form = PostAdminForm()
+        self.assertFalse(form.is_valid())
+
+    def test_form_saves_an_object(self):
+
+        form = PostAdminForm(
+            data={
+                'datetime': self.party_like_its_1999,
+            }
+        )
+
+        self.assertTrue(form.is_valid())
+
+        post = form.save()
+        saved_post = Post.objects.get(id=post.id)
+
+        self.assertEqual(saved_post.datetime, self.party_like_its_1999)
