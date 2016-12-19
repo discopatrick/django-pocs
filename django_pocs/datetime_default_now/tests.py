7a6d167a2b8e997811bfaef64c838ef46aa1bc4f
discopatrick <git@discopatrick.me>
2016-12-19 11:52:19 +0000

    adds Post model with datetime field

--- django_pocs/datetime_default_now/tests.py
+++ django_pocs/datetime_default_now/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
 
-# Create your tests here.
+from datetime import datetime
+from django.utils import timezone
+
+from .models import Post
+
+class PostModelTest(TestCase):
+
+    def test_saving_and_retriving_a_post(self):
+
+        post = Post()
+        post.datetime = timezone.make_aware(datetime(year=1999, month=12, day=31, hour=23, minute=59, second=59))
+        post.save()
+
+        self.assertEqual(Post.objects.count(), 1)
