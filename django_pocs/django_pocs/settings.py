92dd96eb026493cc03b9ca8106d6c03c4b96021e
discopatrick <git@discopatrick.me>
2017-04-30 11:04:15 +0100

    Add endpoints for User model

--- django_pocs/django_pocs/settings.py
+++ django_pocs/django_pocs/settings.py
@@ -105,3 +105,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Django Rest Framework
+# 
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
