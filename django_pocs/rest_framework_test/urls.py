104a6db9d6585c96def36c54f4febe6c1d0f4be6
discopatrick <git@discopatrick.me>
2017-05-01 21:18:05 +0100

    Add api root endpoint

--- django_pocs/rest_framework_test/urls.py
+++ django_pocs/rest_framework_test/urls.py
@@ -3,7 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
 urlpatterns = [
-    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^$', views.api_root),
+    url(r'^snippets/$', views.SnippetList.as_view(), name='snippet-list'),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
 ]
 
